3da5729136e60eef8235bf685a040ed7dad73b41
Raprap <raprapgutierrez095.com>
2026-08-22 00:27:01 +0800

    Fix the space in roomavailability

--- src/pages/Room-Availability/Roomavailability.jsx
+++ src/pages/Room-Availability/Roomavailability.jsx
@@ -2799,7 +2799,7 @@ const RoomAvailability = () => {
             display: "grid",
             gridTemplateColumns: "repeat(auto-fit,minmax(195px,1fr))",
             gap: 14,
-            marginTop: 35,
+            marginTop: 38,
             marginBottom: 20,
           }}
         >
