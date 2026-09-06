6ea7601b10f4b986c7f092a631d26cebc4354088
Raprap <raprapgutierrez095.com>
2026-09-07 03:00:59 +0800

    Fix the logo in register

--- src/pages/Login-Register/Register.jsx
+++ src/pages/Login-Register/Register.jsx
@@ -747,7 +747,7 @@ const Register = () => {
           }}
         >
           <img
-            src="/image/angeles-logo.webp"
+            src="/image/446805041_881106557364617_1125518808684788316_n.webp"
             alt="Logo"
             className="auth-brand-logo"
           />
